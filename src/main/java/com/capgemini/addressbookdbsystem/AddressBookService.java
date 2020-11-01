1e15a22493408232f13af0df14f78e8eec940f19
akshat1032 <akshat.30oct@gmail.com>
2020-11-01 10:44:33 +0530

    Added : Welcome message added

---
+++ src/main/java/com/capgemini/addressbookdbsystem/AddressBookService.java
@@ -0,0 +1,14 @@
+package com.capgemini.addressbookdbsystem;
+
+import java.util.logging.Logger;
+
+public class AddressBookService {
+
+	private static Logger log = Logger.getLogger(AddressBookService.class.getName());
+
+	public static void main(String[] args) {
+
+		// Welcome message added
+		log.info("Welcome to Address Book Database Connectivity");
+	}
+}
